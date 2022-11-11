99943d091aa56e3e9706a48482fcb25383b94172
MateusLopes <mateus20.lopes02@gmail.com>
2022-11-10 22:40:57 -0300

    feat(Slick.js): adicionando carrossel

--- main.js
+++ main.js
@@ -27,12 +27,29 @@ function createCard(date, day, games) {
 }
 
 document.querySelector('#cards').innerHTML =
-    createCard('24/11', 'quinta', createGame('brazil', '16:00', 'serbia')) 
-    +
-    createCard('28/11', 'segunda',
-        createGame('switzerland', '13:00', 'brazil') 
-        +
-        createGame('portugal', '16:00', 'cameroon')
-    ) 
-    +
+    createCard('20/11', 'domingo', createGame('qatar', '13:00', 'ecuador')) +
+    createCard(
+        '21/11',
+        'segunda',
+        createGame('qatar', '13:00', 'ecuador') +
+            createGame('qatar', '13:00', 'ecuador')
+    ) +
+    createCard('24/11', 'quinta', createGame('brazil', '16:00', 'serbia')) +
+    createCard(
+        '28/11',
+        'segunda',
+        createGame('switzerland', '13:00', 'brazil') +
+            createGame('portugal', '16:00', 'cameroon')
+    ) +
     createCard('02/12', 'sexta', createGame('brazil', '16:00', 'serbia'))
+
+$('#cards').slick({
+    rows: 2,
+    gap: 10,
+    dots: false,
+    arrows: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 2
+})
